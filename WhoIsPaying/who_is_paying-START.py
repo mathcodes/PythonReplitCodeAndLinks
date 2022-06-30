ebf93c349b9fc333331f95abd38a199185f69b6a
Jon Christie <jonpchristie@gmail.com>
2022-06-30 01:08:05 -0900

    who is paying start + end

---
+++ WhoIsPaying/who_is_paying-START.py
@@ -0,0 +1,12 @@
+# import random
+
+# Split string method
+# string = '1, 2, 3, 4, 5, 6'
+# split_list = string.split(", ")
+# print(f"When the following two lines of code are executed:\n\n```\nstring = '1, 2, 3, 4, 5, 6'\nsplit_list = string.split(', ')\n``\nI am literally receiving \n```\n{split_list}\n```\n as output.")
+
+names_string = input("Give me everybody's names, seperated by a comma and a space(', ').")
+
+names = names_string.split(", ")
+
+#Write your code below this line 👇
